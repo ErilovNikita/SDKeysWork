1d03d2877b9b18a216700ee5997a919c93f95971
Ерилов Никита <nerilov@octopeople.net>
2026-09-24 00:19:50 +0300

    Update script for pack groovy modules on metainfo

--- scripts/pack-modules.js
+++ scripts/pack-modules.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import fs from 'node:fs/promises'
+import { createHash } from 'node:crypto'
 import path from 'node:path'
 import process from 'node:process'
 
@@ -9,6 +10,7 @@ const modulesDir = path.join(rootDir, 'modules')
 const outputPath = path.resolve(
   process.argv[2] || path.join(rootDir, 'public', 'modules', 'parameters.xml')
 )
+const checksumSalt = '{66d4d9e9390e090854a95dc54e85d27c1eeae80f45083758e379dd34b5a7a83e}\n'
 
 const escapeXml = value => value
   .replaceAll('&', '&amp;')
@@ -18,6 +20,13 @@ const escapeXml = value => value
   .replaceAll("'", '&apos;')
 
 const escapeCdata = value => value.replaceAll(']]>', ']]]]><![CDATA[>')
+const checksum = (code, body) => {
+  const hash = createHash('sha256')
+    .update(`${code}${body}falsefalse${checksumSalt}`, 'utf8')
+    .digest('hex')
+
+  return hash.replace(/^0+(?=.)/, '')
+}
 
 async function getModuleFiles(directory) {
   const entries = await fs.readdir(directory, { withFileTypes: true })
@@ -37,14 +46,13 @@ async function main() {
   const modules = await Promise.all(files.map(async filePath => {
     const body = await fs.readFile(filePath, 'utf8')
     const code = path.basename(filePath, path.extname(filePath))
+    const hash = checksum(code, body)
 
     return [
       '    <module>',
       `      <code>${escapeXml(code)}</code>`,
       '      <active>true</active>',
-      '      <script>',
-      `        <body><![CDATA[${escapeCdata(body)}]]></body>`,
-      '      </script>',
+      `      <script checksum="${hash}"><![CDATA[${escapeCdata(body)}]]></script>`,
       '      <rest_allowed>true</rest_allowed>',
       '    </module>',
     ].join('\n')
